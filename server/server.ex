028204965b2ca23232c86f97ef0c8c0f6ca5dc16
Deepak <sah.sslpu@gmail.com>
2021-04-10 23:54:14 +0530

    add server example

--- server/server.ex
+++ server/server.ex
@@ -1,8 +1,36 @@
-defmodule ServerProcess do
-  def start(callback_module) do
-    spawn(fn ->
-      initial_state = callback_module.init()
-      loop(callback_module, initial_state)
-    end)
+defmodule Server do
+  @moduledoc """
+  Server module defines a long-running server module
+  """
+  @doc """
+  start creates a separate light-weight process and calls loop inside it.
+  """
+  def start() do
+    spawn(fn -> loop end)
+  end
+
+  @doc """
+  send_msg sends a message to server process.
+  """
+  def send_msg(server, message) do
+    send(server, {self(), message})
+
+    receive do
+      {:response, response} -> response
+    end
+  end
+
+  @doc """
+  loop listens for new messages indefinitely.
+  """
+  defp loop() do
+    receive do
+      {caller, msg} ->
+        # Simulate a long running process.
+        Process.sleep(1000)
+        send(caller, {:response, msg})
+    end
+
+    loop()
   end
 end
