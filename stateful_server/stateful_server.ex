00c2f0a900c47338f21e95d7267d9ff1bdfecc3d
Deepak <sah.sslpu@gmail.com>
2021-04-13 23:27:33 +0530

    stateful calculator - add basic structure

---
+++ stateful_server/stateful_server.ex
@@ -0,0 +1,35 @@
+defmodule Calculator do
+  def start do
+    initial_value = 0
+    spawn(fn -> loop(initial_value) end)
+  end
+
+  defp loop(current_value) do
+    new_value =
+      receive do
+        {:value, caller} ->
+          send(caller, current_value)
+          current_value
+
+        {:add, value} -> current_value + value
+        {:sub, value} -> current_value - value
+        {:mul, value} -> current_value * value
+        {:div, value} -> current_value / value
+
+        invalid_request ->
+          IO.puts("invalid request #{inspect invalid_request}")
+          current_value
+      end
+
+    loop(new_value)
+  end
+
+  def value(server_pid) do
+    send(server_pid, {:value, self()})
+    receive do
+      {:response, value} ->
+        value
+    end
+
+  end
+end
