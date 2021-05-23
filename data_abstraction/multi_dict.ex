1ce69d8f6f7f0c057876d2a5a64137588f60bdd2
Deepak <sah.sslpu@gmail.com>
2021-05-23 14:38:26 +0530

    data_abstraction: add comment

--- data_abstraction/multi_dict.ex
+++ data_abstraction/multi_dict.ex
@@ -2,6 +2,7 @@ defmodule MultiDict do
   def new(), do: %{}
 
   def add(dict, key, value) do
+    # using shorthand capture operator for anonymous function.
     Map.update(dict, key, [value], &[value | &1])
   end
 
