d31d478fc024c54321e843232bfd4d6c8020d5cd
Deepak <sah.sslpu@gmail.com>
2021-05-23 14:16:46 +0530

    data_abstraction: refactored TodoList to use MultiDict module

--- data_abstraction/data_abstraction.ex
+++ data_abstraction/data_abstraction.ex
@@ -1,12 +1,13 @@
 defmodule TodoList do
-  def new(), do: %{}
+  import MultiDict
+
+  def new(), do: MultiDict.new()
 
   def add_entry(todo_list, date, title) do
-    Map.update(
-      todo_list,
-      date,
-      [title],
-      fn titles -> [title | titles] end
-    )
+    MultiDict.add(todo_list, date, title)
+  end
+
+  def entries(todo_list, date) do
+    MultiDict.get(todo_list, date)
   end
 end
