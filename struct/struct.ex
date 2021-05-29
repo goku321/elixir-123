a459eb0a036903efd4e5b7e349e509c699e0a52d
Deepak <sah.sslpu@gmail.com>
2021-05-29 15:32:14 +0530

    struct: add constructor

--- struct/struct.ex
+++ struct/struct.ex
@@ -2,4 +2,8 @@ defmodule Fraction do
   # each module can only have one struct.
   # a struct may exist only in a module.
   defstruct a: nil, b: nil
+
+  def new(a, b) do
+    %Fraction{a: a, b: b}
+  end
 end
