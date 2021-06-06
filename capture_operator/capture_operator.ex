b08528eed1a623320400ffc4f579693a862f3a54
Deepak <sah.sslpu@gmail.com>
2021-06-06 17:28:09 +0530

    capture_operator: add another example

--- capture_operator/capture_operator.ex
+++ capture_operator/capture_operator.ex
@@ -1,7 +1,14 @@
 defmodule CaptureOperator do
   def add_one(x) do
+    # usage 1 - capturing an anonymous function.
     # fn is analogous to fn x -> x + 1 end
     func = &(&1+1)
     func.(x)
+  end
+
+  def print(x) do
+    # usage 2 - capturing a function with a given name and arity.
+    p = &(IO.puts/1)
+    p.(x)
   end
 end
