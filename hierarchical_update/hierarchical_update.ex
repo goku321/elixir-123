8eeeee6e805c60d98a9e2fe57bb8447ab6daed96
Deepak <sah.sslpu@gmail.com>
2021-07-01 23:59:39 +0530

    hierarchical_update: add comments

--- hierarchical_update/hierarchical_update.ex
+++ hierarchical_update/hierarchical_update.ex
@@ -1,4 +1,5 @@
 defmodule HierarchicalUpdate do
+  # new_map returns a nested map data structure.
   def new_map() do
     %{
       1 => %{name: "Luke", age: 26},
@@ -6,6 +7,11 @@ defmodule HierarchicalUpdate do
     }
   end
 
+  # updates the given by using Kernel built-in method.
+  # put_in traverses the structure to the given path
+  # and updates the target value.
+  # this update is still immutable, it does not mutate the
+  # original data structure.
   def update_age_by_id(%{} = map, id, age) do
     put_in(map[id].age, age)
   end
