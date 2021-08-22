f515be778df8a36c6759d6fe5ef4a21b1da67698
Deepak <sah.sslpu@gmail.com>
2021-08-22 18:09:47 +0530

    enum_reduce: use Enum.reduce to transform a given list

--- enum_reduce/enum_reduce.ex
+++ enum_reduce/enum_reduce.ex
@@ -1,2 +1,17 @@
 defmodule EnumReduce do
+  import Person
+
+  # returns a list of Person
+  # entries is a list of key/value pairs
+  # entries = [%{:name => "jabba", :age => 1000}, %{:name => "luke", :age => 30}]
+  def create_person_list(entries \\ []) do
+    Enum.reduce(
+      entries,
+      [],
+      fn entry, acc ->
+        p = Person.new(entry.name, entry.age)
+        acc = [p | acc]
+      end
+    )
+  end
 end
--- enum_reduce/person.ex
+++ enum_reduce/person.ex
@@ -1,6 +1,7 @@
 defmodule Person do
   defstruct name: nil, age: nil
 
+  # new creates a person struct with given name and age.
   def new(name, age) do
     %Person{name: name, age: age}
   end
